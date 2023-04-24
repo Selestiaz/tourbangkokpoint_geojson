1b8251290649e97cc8d8b04c1f6ef2230055fe90
pichayut <124547965+Selestiaz@users.noreply.github.com>
2023-04-24 10:41:33 +0700

    Add files via upload

---
+++ tourpoint_bangkok.js
@@ -0,0 +1,155 @@
+var tourpointbangkok = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Lumphini Park",
+          "type": "Park",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.54167509908211,
+            13.731155918414956
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "The Royal bangkok sports club",
+          "type": "Sport club",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.5373551976993,
+            13.738980521070559
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "MBK center",
+          "type": "Super market",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.52987000316296,
+            13.744360456289996
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Chulalongkorn University Stadium",
+          "type": "University",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.52570981692173,
+            13.737287576546677
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Sa Pathum Palace",
+          "type": " Royal residence",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.5328777396465,
+            13.748164775673061
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "หอประชุมพันธมิตรฝรั่ง",
+          "type": "Auditorium",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.54835328255444,
+            13.729569239425885
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Benjakitti Forest Park",
+          "type": "Park",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.55494102880522,
+            13.730222707219085
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Tesco Lotus",
+          "type": "Super Market",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.56853170577875,
+            13.717250876110398
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Arun Ratchawararam Ratchaworamahawihan",
+          "type": "Temple",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.4883835719549,
+            13.743712254129662
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat pho",
+          "type": "Temple",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.49305059698696,
+            13.745854938903975
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
